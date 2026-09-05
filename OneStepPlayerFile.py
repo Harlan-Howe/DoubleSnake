aeb1f0be284730016095f55802cb081865fddfb1
Harlan Howe <harlan.howe@kinkaid.org>
2026-09-05 16:40:12 -0500

    reordered selectMove()

--- OneStepPlayerFile.py
+++ OneStepPlayerFile.py
@@ -1,52 +1,66 @@
+from typing import Callable, Optional, List
+
 from PlayerFile import Player
-from DSBoard import Board
+from DSBoard import Board, Move
+
 
 class OneStepPlayer(Player):
     """
     A short-sighted player - it picks the move that will maximize the number of runs and/or captures this ply.
     """
-
-
     def __init__(self):
-        pass
+        super().__init__()
 
-    def select_move(self, board, which_player_am_I, get_expired_time_method,opponents_move = None):
-        #start with a random move...
-        best_move = Player.select_move(self, board=board,
-                                       which_player_am_I=which_player_am_I,
-                                       get_expired_time_method=get_expired_time_method)
+    def select_move(self,
+                    board: Board,
+                    which_player_am_I: int,
+                    get_expired_time_method: Callable,
+                    opponents_move: Optional[Move] = None) -> Move:
+
+        # what moves are even options?
+        potential_moves: List[Move] = board.get_possible_moves(randomize=True)[which_player_am_I]
+
+        # start with a random move...
+        best_move = potential_moves[0]
         best_score = 0
-        #... and hopefully, we can do better.
-        potential_moves = board.get_possible_moves(randomize=True)[which_player_am_I]
+
+        # ... and hopefully, we can do better.
+
         for move in potential_moves:
-            #bail out if we're about to run out of time....
-            if get_expired_time_method()[1] < 0.01: # You can adjust this time (in seconds) to refine how close you wish to cut it....
+            # Bail out if we're about to run out of time....
+            if get_expired_time_method()[1] < 0.01:  # You can adjust this time (in seconds) to refine how close you
+                #                                      wish to cut it....
                 break
 
             score = self.get_score_for_move(board, which_player_am_I, move)
 
-            if score>best_score:
+            if score > best_score:
                 best_score = score
                 best_move = move
         return best_move
 
-    def get_score_for_move(self, board, which_player, move):
+    def get_score_for_move(self,
+                           board: Board,
+                           which_player: int,
+                           move: Move):
         """
         This is a blunt method for finding the score, and it does not take into account anything other than the move
         selected. In this simple example, it returns the number of moves this player would have available on the next
         turn... if this player got to move again immediately.
         :param board:
         :param which_player:
-        :param move_loc:
+        :param move:
         :return:
         """
         # What would this board look like if you made this move?
-        board_copy = Board(board_to_copy = board)
+        board_copy = Board(board_to_copy=board)
         board_copy.make_move_for_player(move, which_player)
 
         return self.score_for_board(board_copy, which_player_am_I=which_player)
 
-    def score_for_board(self, board: Board, which_player_am_I: int = 0) -> int:
+    def score_for_board(self,
+                        board: Board,
+                        which_player_am_I: int = 0) -> int:
         """
         bases the score on the number of possible moves this player has remaining.
         :param board:
